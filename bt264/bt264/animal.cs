92ed4a56ccac37f787d315411a9e8928f8a4d70c
luongvinhlong9 <80777052+luongvinhlong9@users.noreply.github.com>
2022-04-26 07:59:21 +0700

    Bt 26/4

---
+++ bt264/bt264/animal.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bt264
+{
+    class animal
+    {
+        public virtual void eat()
+        {           
+        }
+        public virtual void makeSound()
+        {
+        }
+    }
+    class Cat:animal
+    {
+        public override void eat()
+        {
+            Console.WriteLine("Cat eat fisch");
+        }
+        public override void makeSound()
+        {
+            Console.WriteLine("Meow meow");
+        }
+        public void run()
+        {
+            Console.WriteLine("By 4 legs");
+        }
+    }
+    class bird : animal
+    {
+        public override void eat()
+        {
+            Console.WriteLine("Cat eat rice");
+        }
+        public override void makeSound()
+        {
+            Console.WriteLine("Chip chip");
+        }
+        public void fly()
+        {
+            Console.WriteLine("By 2 wings");
+        }
+    }
+}
---
+++ bt264/bt264/ptBacHai.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+class ptBacHai
+{
+    private int a, b, c;
+    public ptBacHai()
+    {
+
+        a = int.Parse(Console.ReadLine());
+        b = int.Parse(Console.ReadLine());
+        c = int.Parse(Console.ReadLine());
+    }
+    public ptBacHai(int a1, int b1, int c1)
+    {
+        a = a1;
+        b = b1;
+        c = c1;
+    }
+    ~ptBacHai() { }
+    public int gsa
+    {
+        get { return this.a; }
+        set { this.a = value; }
+    }
+    public int gsb
+    {
+        get { return this.b; }
+        set { this.b = value; }
+    }
+    public int gsc
+    {
+        get { return this.c; }
+        set { this.c = value; }
+    }
+    public double delta() {
+        double delt;
+        delt = Math.Pow(b, 2) - 4 * a * c;
+        return delt;
+    }
+    public void nghiem()
+    {
+        
+        if (delta() < 0)
+        {
+            Console.WriteLine("Vo nghiem");
+        }
+        else if(delta() == 0)
+        {
+            double x = (-b) / (2 * a);
+        }
+        else
+        {
+            double x1= (-b-Math.Sqrt(delta())) / (2 * a);
+            double x2 = (-b - Math.Sqrt(delta())) / (2 * a);
+        }
+    }
+    
+}
