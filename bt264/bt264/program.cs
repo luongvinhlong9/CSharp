de5bfe23353ea9a268ca3a216bc788d4b3cb050c
luongvinhlong9 <80777052+luongvinhlong9@users.noreply.github.com>
2022-04-26 08:07:46 +0700

    Add files via upload

---
+++ bt264/bt264/program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bt264
+{
+    internal class program
+    {
+        public void Main(String[] args)
+        {
+            int a=int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+            ptBacHai pt = new ptBacHai();
+            pt.nghiem();
+            Cat cat = new Cat();
+            cat.eat();
+            cat.makeSound();
+            cat.run();
+            Bird bird = new Bird();
+            bird.eat();
+            bird.makeSound();
+            bird.fly();
+            Console.ReadKey();
+        }
+    }
+}
