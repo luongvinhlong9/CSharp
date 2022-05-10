1f493791e22ad8c809c58e2a18ae8959ba64594c
luongvinhlong9 <80777052+luongvinhlong9@users.noreply.github.com>
2022-05-10 07:40:45 +0700

    BT 20220510

---
+++ bt1005/bt1005/Library.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bt1005
+{
+    internal class Library
+    {
+        public int findMax(int[] array)
+        {
+            int max=array[0];
+            for(int i=1; i<array.Length; i++)
+            {
+                if(max<array[i])
+                    max=array[i];
+            }
+            return max;
+        }
+
+        public int findMin(int[] array)
+        {
+            int min = array[0];
+            for (int i = 1; i < array.Length; i++)
+            {
+                if (min > array[i])
+                    min = array[i];
+            }
+            return min;
+        }
+
+        public void findMaxApear(int[] array)
+        {
+            int[] tempArray;
+            int MaxApear = array[0], Apear=0;
+            for (int i = 0; i < array.Length; i++) {
+                for (int j = 0; i <= tempArray.Length; j++)
+                {
+                    if (array[i] == tempArray[j])
+                        continue;
+                    else
+                    {
+
+                        if ()
+                    }
+                }
+            }
+
+
+        }   
+    }
+}
---
+++ bt1005/bt1005/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bt1005
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array35= new int[10];
+            Console.WriteLine("Nhap 10 phan tu cua day: ");
+            for(int i35=0; i35 < 10; i35++)
+            {
+                array35[i35] = int.Parse(Console.ReadLine());
+            }
+            Library lib35 = new Library();
+            Console.WriteLine("Gia tri lon nhat la: {0}", lib35.findMax(array35));
+            Console.WriteLine("Gia tri be nhat la: {0}", lib35.findMax(array35));
+        }
+    }
+}
